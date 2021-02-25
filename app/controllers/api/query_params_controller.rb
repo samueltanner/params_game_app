24d66c4a16072dddaf731bf1700cd6192327edcd
Sam Tanner <Samuel.m.s.tanner@gmail.com>
2021-02-25 12:22:10 -0800

    adds the ability to check the first name

--- app/controllers/api/query_params_controller.rb
+++ app/controllers/api/query_params_controller.rb
@@ -1,10 +1,17 @@
 class Api::QueryParamsController < ApplicationController
   def name_checker_action
     input_name = params["my_name"]
+    #@output_message = "this was put in #{input_name}"
     if input_name[0] == "a"
       @output_message = "aaaaye"
+    else
+      @output_message = "nada"
     end
+
+    render "name_checker.json.jb"
   end
 
-  render "name_checker.json.jb"
+  def num_guesser_action
+    @response = "this will be a number"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/name_checker" => "query_params#name_checker_action"
+
+    get "/num_guesser" => "query_params#num_guesser_action"
   end
 end
